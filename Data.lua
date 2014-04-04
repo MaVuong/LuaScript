13b76b359ae36edec8720e2df10d1fb4a973d8bf
MaVuong <dethichthien@me.com>
2014-04-04 16:54:45 +0700

    v2
    Moi

--- Data.lua
+++ Data.lua
@@ -15,3 +15,27 @@ function LKS_Vdict(strSed)
     lks_ketquacut(ketquatrave,'"xong"');
 end
 
+function LKS_WikiDict(strSed)
+    str1='Cách phát âm';
+    str2='<div class="NavContent">';
+
+    v1=string.find(strSed,str1);
+    v2=string.find(strSed,str2);
+    print(v1);
+    print(v2);
+    ketquatrave = string.sub(strSed, v1+102, v2-1);
+    print(tostring(ketquatrave))  ;
+    
+
+    dodai=string.len(tostring(ketquatrave));
+      if dodai <10 then
+        ketquatrave="Error";
+      end
+    lks_ketquacut(ketquatrave,'"xong"');
+end
+
+
+
+
+
+
